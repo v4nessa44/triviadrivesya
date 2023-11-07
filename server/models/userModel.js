00f7b89ac174ae5347005c410ac1b70202636ba5
tijanihind <tijani.hind200@gmail.com>
2023-11-06 20:07:47 -0500

    added server folder

---
+++ server/models/userModel.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const bycrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// check password function for a specific user
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  /* compare between passwords using bycrypt 
+   NOTE: bcrypt hashes the entered password before comparing as the
+   password stored in the DB is hashed
+  */
+  return await bycrypt.compare(enteredPassword, this.password);
+};
+
+// calls function before saving so we bcrypt the password before saving
+userSchema.pre("save", async function (next) {
+  // check if password is sent or modified
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bycrypt.genSalt(10);
+  // hash password with salt
+  this.password = await bycrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
---
+++ server/routes/api/userRoutes.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const {
+  authUser,
+  deleteUser,
+  getUserById,
+  getUserProfile,
+  getUsers,
+  registerUser,
+  updateUser,
+  updateUserProfile,
+} = require("../controllers/userController.js");
+const { admin, protect } = require("../middleware/authMiddleware.js");
+
+const router = express.Router();
+
+// @desc Register a new user
+// @route POST /api/users
+// @access Public
+router.route("/").post(registerUser);
+
+// @desc Authenticate user & get token
+// @route POST /api/users/login
+// @access Public
+router.route("/login").post(authUser);
+
+// @desc Get user profile
+// @route GET /api/users/profile
+// @access Private
+// protect middleware is going to run whenever we hit /api/users/profile
+router.route("/profile").get(protect, getUserProfile);
+
+// @desc Get all users - admins only
+// @route GET /api/users
+// @access Private/Admin
+/* this route goes through two middlewares protect and admin, making sure
+    the user is both loged in and is an admin before he can access this
+    route 
+*/
+router.route("/").get(protect, admin, getUsers);
+
+// @desc Update user profile
+// @route PUT /api/users/profile
+// @access Private
+router.route("/profile").put(protect, updateUserProfile);
+
+// @desc delete user
+// @route DELETE /api/users/:id
+// @access Private/Admin
+router.route("/:id").delete(protect, admin, deleteUser);
+
+// @desc Get user by ID
+// @route GET /api/users/:id
+// @access Private/Admin
+router.route("/:id").get(protect, admin, getUserById);
+
+// @desc Update user
+// @route PUT /api/users/:id
+// @access Private/Admin
+router.route("/:id").put(protect, admin, updateUser);
+
+module.exports = router;
