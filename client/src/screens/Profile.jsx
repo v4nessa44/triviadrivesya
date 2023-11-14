97b405f32c55d401d4fa2d65fafff2fa5b8b963a
Mukunth Arjunan <mukunth.arjunan@gmail.com>
2023-11-13 19:35:51 -0500

    adding favorite function to profile

--- client/src/screens/Profile.jsx
+++ client/src/screens/Profile.jsx
@@ -2,9 +2,8 @@ import React, { useState, useEffect } from "react";
 
 function Profile() {
   const [gameHistory, setGameHistory] = useState([]);
-  
-  // Assuming you have the user ID available, replace 'yourUserId' with the actual user ID
-  const userId = 'yourUserId';
+  const [favoriteCategory, setFavoriteCategory] = useState(null);
+  const [userId, setUserId] = useState('yourUserId'); // Replace with actual user ID
 
   useEffect(() => {
     // Fetch user's game history
@@ -12,7 +11,8 @@ function Profile() {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-
+        // Include authentication token if required
+        // "Authorization": `Bearer ${yourAuthToken}`,
       },
     })
       .then((res) => res.json())
@@ -23,6 +23,30 @@ function Profile() {
         console.error("Error fetching game history:", error);
       });
   }, [userId]);
+
+  const handleFavoriteCategory = (category) => {
+    // Send a request to mark the category as a favorite in the backend
+    fetch(`http://localhost:3001/mark-favorite-category`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // Include authentication token if required
+        // "Authorization": `Bearer ${yourAuthToken}`,
+      },
+      body: JSON.stringify({
+        userId: userId,
+        category: category,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Category marked as favorite:", data);
+        setFavoriteCategory(category);
+      })
+      .catch((error) => {
+        console.error("Error marking category as favorite:", error);
+      });
+  };
 
   return (
     <div>
@@ -35,6 +59,11 @@ function Profile() {
             <li key={index}>
               <p>Category: {game.category}</p>
               <p>Score: {game.score}</p>
+              {favoriteCategory !== game.category && (
+                <button onClick={() => handleFavoriteCategory(game.category)}>
+                  Mark as Favorite
+                </button>
+              )}
             </li>
           ))}
         </ul>
@@ -44,3 +73,4 @@ function Profile() {
 }
 
 export default Profile;
+
