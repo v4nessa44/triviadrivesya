30dca75d260f53ecc019c717c66507f16615267f
Mukunth Arjunan <mukunth.arjunan@gmail.com>
2023-11-16 01:08:47 -0500

    changing config

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+require('dotenv').config()
 
-dotenv.config({
-    path: './config.env'
-});
-
-const mongourl = 'mongodb+srv://admin:admin321@cluster0.nmdv2ie.mongodb.net/techmatchup?retryWrites=true&w=majority';
+const mongourl = process.env.MONGODB_URI
 mongoose.connect(mongourl);
 
 module.exports = mongoose.connection;
